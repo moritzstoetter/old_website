19b20091937bb7ddd6b86ef7844790bb3d73ed15
Moritz Stötter <hi@moritzstoetter.dev>
2024-02-24 21:19:29 +0100

    added quote, fixed some minor issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -181,22 +181,22 @@ const setLang = (languageCode) => {
     });
   }
 
-  /**
-   * Skills animation
-   */
-  let skillsContent = select('.skills-content');
-  if (skillsContent) {
-    new Waypoint({
-      element: skillsContent,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = select('.progress .progress-bar', true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
-        });
-      }
-    })
-  }
+  // /**
+  //  * Skills animation
+  //  */
+  // let skillsContent = select('.skills-content');
+  // if (skillsContent) {
+  //   new Waypoint({
+  //     element: skillsContent,
+  //     offset: '80%',
+  //     handler: function(direction) {
+  //       let progress = select('.progress .progress-bar', true);
+  //       progress.forEach((el) => {
+  //         el.style.width = el.getAttribute('aria-valuenow') + '%'
+  //       });
+  //     }
+  //   })
+  // }
 
   /**
    * Porfolio filter
@@ -256,7 +256,8 @@ const setLang = (languageCode) => {
   const portfolioDetailsLightbox = GLightbox({
     selector: '.portfolio-details-lightbox',
     width: '90%',
-    height: '90vh'
+    height: '90vh',
+    openEffect: 'zoom'
   });
 
   /**
